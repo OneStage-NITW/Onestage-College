200ed4dc91b523250ace47aea98155f2f3ab2ebc
acemaster <vivekhtc25@gmail.com>
2016-02-28 13:27:54 +0530

    Added new designs.

--- homeapp/views.py
+++ homeapp/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import User
 
 
 def home(request):
-	return render(request,'homeapp/site/home.html',{})
+	return render(request,'homeappmaterial/site/home.djt',{})
 
 def rendercappage(request,capid):
 	"""We will be redering the cap page here. working on it """
